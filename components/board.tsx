f9782202e32e93400fd8459f90f5fcb9caa284af
darkdulgi <daniel99285@gmail.com>
2024-01-13 13:36:34 +0900

    1/13: 초기화 함수 분리, 실패 시 모든 지뢰 보기와 비활성화

--- components/board.tsx
+++ components/board.tsx
@@ -1,34 +1,18 @@
 import { useEffect, useState, memo } from "react";
+import initialize from "@/components/initialize";
+
 const dx = [-1, -1, -1, 0, 1, 1, 1, 0];
 const dy = [-1, 0, 1, 1, 1, 0, -1, -1];
 
-const initialize = (allMine, setField, setView, row, col) => {
-  const temp = new Array(row * col).fill(0);
-  const tempField = new Array(row).fill(0).map(() => new Array(col).fill(0));
-  setView(tempField.map(arr => [...arr]));
-  for (let i = 0; i < allMine; i++) temp[i] = -1;
-  for (let i = 0; i < row * col; i++) {   // 배열 무작위 섞기(Fisher-Yates shuffle)
-    let j = Math.floor(Math.random() * (i + 1));
-    [temp[i], temp[j]] = [temp[j], temp[i]];
-  }
+const seeAllMine = (field, view, setView, setFailed, row, col) => {
+  const copyView = view.map(arr => [...arr]);
   for (let i = 0; i < row; i++) {
     for (let j = 0; j < col; j++) {
-      tempField[i][j] = temp[i * col + j];
+      if (field[i][j] === -1) copyView[i][j] = 1;
     }
   }
-  for (let i = 0; i < row; i++) {
-    for (let j = 0; j < col; j++) {
-      if (tempField[i][j] === -1) continue;
-      let nearMine = 0;
-      for (let k = 0; k < 8; k++) {
-        let x = i + dx[k];
-        let y = j + dy[k];
-        if (x >= 0 && x < row && y >= 0 && y < col && tempField[x][y] === -1) nearMine++;
-      }
-      tempField[i][j] = nearMine;
-    }
-  }
-  setField(tempField.map(arr => [...arr]));
+  setView(copyView.map(arr => [...arr]));
+  setFailed(true);
 }
 
 const Board = ({ row, col }) => {
@@ -36,12 +20,18 @@ const Board = ({ row, col }) => {
   const [field, setField] = useState([]);
   const [view, setView] = useState([]);
   const [mine, setMine] = useState<number>(allMine);
+  const [failed, setFailed] = useState(false);
 
   useEffect(() => {
-    initialize(allMine, setField, setView, row, col);
+    initialize(setField, setView, setFailed, allMine, row, col);
   }, [row])
 
   const clickHandler = (i, j) => {
+    if (field[i][j] === -1) {
+      alert('실패!');
+      seeAllMine(field, view, setView, setFailed, row, col);
+      return;
+    }
     const copyView = view.map(arr => [...arr]);
     const q = [];
     q.push({ x: i, y: j });
@@ -66,15 +56,16 @@ const Board = ({ row, col }) => {
           {oneRow.map((room, j) => (
             <button
               key={j}
-              className={`h-10 w-10 border border-black ${room ? 'bg-white' : 'bg-slate-400'}`}
-              onClick={(e) => clickHandler(i, j)}>
-              {room ? field[i][j] : null}
+              disabled={failed}
+              className={`h-10 w-10 border border-black ${room ? (field[i][j] === -1 ? 'bg-red-300' : 'bg-white') : 'bg-slate-400'}`}
+              onClick={() => clickHandler(i, j)}>
+              {room ? (field[i][j] !== 0 ? (field[i][j] === -1 ? <img src='/mine.png' /> : field[i][j]) : null) : null}
             </button>
           ))}
         </div>
       ))}
 
-      <button className="bg-green-200" onClick={() => initialize(allMine, setField, setView, row, col)}>
+      <button className="bg-green-200" onClick={() => initialize(setField, setView, setFailed, allMine, row, col)}>
         다시하기
       </button>
     </div>
---
+++ components/initialize.tsx
@@ -0,0 +1,32 @@
+const dx = [-1, -1, -1, 0, 1, 1, 1, 0];
+const dy = [-1, 0, 1, 1, 1, 0, -1, -1];
+
+export default function initialize (setField, setView, setFailed, allMine, row, col) {
+  const temp = new Array(row * col).fill(0);
+  const tempField = new Array(row).fill(0).map(() => new Array(col).fill(0));
+  setView(tempField.map(arr => [...arr]));
+  setFailed(false);
+  for (let i = 0; i < allMine; i++) temp[i] = -1;
+  for (let i = 0; i < row * col; i++) {   // 배열 무작위 섞기(Fisher-Yates shuffle)
+    let j = Math.floor(Math.random() * (i + 1));
+    [temp[i], temp[j]] = [temp[j], temp[i]];
+  }
+  for (let i = 0; i < row; i++) {
+    for (let j = 0; j < col; j++) {
+      tempField[i][j] = temp[i * col + j];
+    }
+  }
+  for (let i = 0; i < row; i++) {
+    for (let j = 0; j < col; j++) {
+      if (tempField[i][j] === -1) continue;
+      let nearMine = 0;
+      for (let k = 0; k < 8; k++) {
+        let x = i + dx[k];
+        let y = j + dy[k];
+        if (x >= 0 && x < row && y >= 0 && y < col && tempField[x][y] === -1) nearMine++;
+      }
+      tempField[i][j] = nearMine;
+    }
+  }
+  setField(tempField.map(arr => [...arr]));
+}
