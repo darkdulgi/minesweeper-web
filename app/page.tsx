6157279605348a7f293e2eade8e23040944dbbfd
darkdulgi <daniel99285@gmail.com>
2024-01-19 20:07:08 +0900

    1/19: 스타일링 약간

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,9 @@ import { useState } from "react";
 
 export default function Page() {
   const difficulty = [
-    { row: 9, col: 9, name: 'easy', id: 0 },
-    { row: 18, col: 18, name: 'medium', id: 1 },
-    { row: 18, col: 30, name: 'hard', id: 2 },
+    { row: 9, col: 9, name: 'Easy', id: 0 },
+    { row: 18, col: 18, name: 'Medium', id: 1 },
+    { row: 18, col: 30, name: 'Hard', id: 2 },
   ];
   const [stage, setStage] = useState<number>(0);
   const [selectStage, setSelectStage] = useState<number>(0);
@@ -24,33 +24,43 @@ export default function Page() {
           Minesweeper
         </div>
 
-        <div>
+        <div className="flex items-center gap-3">
           <span>
             크기 선택
           </span>
-
-          {difficulty.map(game => (
-            <label key={game.id}>
-              <input
-                type="radio"
-                value={game.id}
-                checked={selectStage === game.id}
-                onChange={handleRadio}
-              />
-              {game.row}x{game.col}
-            </label>
-          ))}
+          <div className="flex flex-col">
+            {difficulty.map(game => (
+              <label key={game.id}>
+                <input
+                  type="radio"
+                  className="mr-1"
+                  value={game.id}
+                  checked={selectStage === game.id}
+                  onChange={handleRadio}
+                />
+                {game.row} x {game.col}
+              </label>
+            ))}
+          </div>
 
           <input
             type='button'
             value='Go!'
-            className="border bg-sky-100 active:bg-red-100"
+            className="bg-sky-200 text-gray-600 font-bold px-3 py-1 rounded-lg hover:bg-red-200 active:bg-red-400 transition ease-in-out"
             onClick={handleClick}
           />
         </div>
 
-        현재 난이도:{difficulty[stage].name}
+        <div>
+          <span className="text-gray-500">
+            {`현재 난이도 : `}
+          </span>
 
+          <span className={`font-bold ${stage === 0 ? 'text-green-500':(stage === 1 ? 'text-blue-700' : 'text-red-700')}`}>
+            {difficulty[stage].name}
+          </span>
+        </div>
+        
         <Board row={difficulty[stage].row} col={difficulty[stage].col} />
       </div>
     </div>
